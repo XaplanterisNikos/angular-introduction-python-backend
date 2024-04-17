ff6a89feed4389c34d29f9ec0da5f477d08f11d8
N. Xaplanteris <xaplanterisnikos@gmail.com>
2024-04-17 15:21:04 +0300

    mongodb

--- src/__init__.py
+++ src/__init__.py
@@ -11,7 +11,7 @@ jwt = JWTManager(app)
 app.config["JWT_SECRET_KEY"] = "super secret and difficult to guess key"
 
 connect(
-    host="mongodb+srv://coding-factory:ufTXpw9bAEejrXfb@cluster0.okry00y.mongodb.net",
+    host="mongodb+srv://cfuser:12345@cluster0.s8gapm5.mongodb.net/",
     db="Coding-Factory",
     alias="coding-factory",
 )
